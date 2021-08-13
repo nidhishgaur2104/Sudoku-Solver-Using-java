7f62e58051fbd84ac44d612a010af1f2b10ba4d6
nidhishgaur2104 <87849227+nidhishgaur2104@users.noreply.github.com>
2021-08-13 19:47:40 +0530

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Sudoku {
   public static void display(int[][] board){
     for(int i = 0; i < board.length; i++){
       for(int j = 0; j < board[0].length; j++){
